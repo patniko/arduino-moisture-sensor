5a042b34ee8c71e473c8b7b6d8e68fe555b311fb
Patrick Nikoletich <pnikoletich@gmail.com>
2018-04-25 22:14:16 -0700

    HTTP trackEvent from Arduino

--- arduino/sensor/sensor.ino
+++ arduino/sensor/sensor.ino
@@ -50,28 +50,3 @@ void loop() {
   delay(300);
 }
 
-void msg(String value)
-{
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print(value);
-}
-void msg2(String value)
-{
-  lcd.setCursor(0,1);
-  lcd.print(value);
-}
-
-void displayReading(String value) {
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print("Zone 1: ");
-  lcd.setCursor(8,0);
-  
-  char percent [6]; 
-  sprintf(percent, "%s%%", String(soilMoisturePercentage).c_str());
-  lcd.print(percent);
-
-  lcd.setCursor(0,1);
-  lcd.print(value);
-}
--- arduino/sensor/wifi.ino
+++ arduino/sensor/wifi.ino
@@ -12,7 +12,7 @@ Adafruit_CC3000 cc3000 = Adafruit_CC3000(ADAFRUIT_CC3000_CS, ADAFRUIT_CC3000_IRQ
                                          SPI_CLOCK_DIVIDER);
 Adafruit_CC3000_Client
   client;        // For WiFi connections
-
+#define IDLE_TIMEOUT_MS  3000
 
 unsigned long
   currentTime = 0L;
@@ -61,6 +61,8 @@ void connectWifi() {
   msg2("Connected!");
   delay(1000);
   getTime();
+
+trackEvent(24);
 }
 
 void listSSIDResults(void)
@@ -163,5 +165,49 @@ unsigned long getTime(void) {
   }
   if(!t) Serial.println("error");
   return t;
+}
+
+
+void trackEvent(float value) {
+  uint32_t ip = 0;
+  // Try looking up the website's IP address
+  Serial.print("42265d68.ngrok.io"); Serial.print(F(" -> "));
+  while (ip == 0) {
+    if (! cc3000.getHostByName("42265d68.ngrok.io", &ip)) {
+      Serial.println(F("Couldn't resolve!"));
+    }
+    delay(500);
+  }
+  
+  Adafruit_CC3000_Client www = cc3000.connectTCP(ip, 80);
+  if (www.connected()) {
+    www.fastrprint(F("GET "));
+    www.fastrprint("/api/TrackMetric/asdf/-80.4");
+    www.fastrprint(F(" HTTP/1.1\r\n"));
+    www.fastrprint(F("Host: ")); www.fastrprint("42265d68.ngrok.io"); www.fastrprint(F("\r\n"));
+    www.fastrprint(F("\r\n"));
+    www.println();
+  } else {
+    Serial.println(F("Connection failed"));    
+    return;
+  }
+  
+  Serial.println(F("-------------------------------------"));
+  
+  /* Read data until either the connection is closed, or the idle timeout is reached. */ 
+  unsigned long lastRead = millis();
+  while (www.connected() && (millis() - lastRead < IDLE_TIMEOUT_MS)) {
+    while (www.available()) {
+      char c = www.read();
+      Serial.print(c);
+      lastRead = millis();
+    }
+  }
+  www.close();
+  Serial.println(F("-------------------------------------"));
+  
+  /* You need to make sure to clean up after yourself or the CC3000 can freak out */
+  /* the next time your try to connect ... */
+  Serial.println(F("\n\nDisconnecting"));
 }
 
